2eefbc610379aab88c3d41cb545429423c0b26f8
Johannes Voll Kolstø <johannesvk@protonmail.com>
2020-09-14 17:00:28 +0200

    Further analysis. Trying to establish stationarity in our timeseries for forecasting

--- project-1/project1_johannes.r
+++ project-1/project1_johannes.r
@@ -1,33 +1,42 @@
 library(forecast)
 
+# Load data:
 cv19 = read.csv2("covid19.csv", header=TRUE, sep=";")
-
 colnames(cv19) <- c("Dato", "Kum.Ant", "Nye.Tilf")
 cv19$Dato = as.Date(cv19$Dato, "%d.%m.%y")
 
-# Looking at what transformations of our data could result
-# in what appears to be a stationary process:
-hist(cv19$Nye.Tilf)
-cv19$Nye.Tilf.Box_Cox = BoxCox(cv19$Nye.Tilf, 0.33)
-Nye.Tilf_bc_lm = lm()
-ggtsdisplay(box_cox_transformed_Nye.Tlf)
+# Difference the new cases before polynomial fitting:
+Nye.Tilf.diff7 = diff(cv19$Nye.Tilf, differences=1, lag=7)
+Dato.diff7 = cv19$Dato[8:length(cv19$Dato)]
+lm_p4_Nye.Tilf = lm(Nye.Tilf.diff7~poly(Dato.diff7, degree=4))
+
+# Shift residuals so that min(Nye.Tilf$residuals) = 1.0 if
+# there were negative values present before.
+Nye.Tilf_res_min_value = min(lm_p4_Nye.Tilf$residuals)
+Nye.Tilf.shifted_res = lm_p4_Nye.Tilf$residuals - min(c(0, Nye.Tilf_res_min_value)) + 1
+Nye.Tilf_res_lambda = BoxCox.lambda(cv19$Nye.Tilf.shifted_res)
+Nye.Tilf.BXCX_Res = BoxCox(lm_p4_Nye.Tilf$residuals, Nye.Tilf_res_lambda)
+
+ggtsdisplay(Nye.Tilf.BXCX_Res)
 
 hist(box_cox_transformed_Nye.Tlf)
 plot(cv19$Dato, box_cox_transformed_Nye.Tlf)
 
 # Beginning experimentation with ARIMA-model:
-arma_mod = Arima(cv19$Nye.Tilf - mean(cv19$Nye.Tilf), order=c(3, 1, 3))
+arma_mod = Arima(Nye.Tilf.BXCX_Res, order=c(3, 0, 3))
 str(arma_mod)
+# Extract AICC:
+arma_mod$aicc
+
 accuracy(arma_mod)
 plot(arma_mod)
 
-plot(cv19$Dato, mean_cor)
 ggtsdisplay(cv19$Nye.Tilfeller - mean(cv19$Nye.Tilfeller))
 str(arma_mod)
 
-typeof(cv19)
-cv19ts = as.ts(cv19, deltat=1/365)
-str(cv19ts)
-summary(cv19ts)
-plot(cv19$Dato, log(cv19$Nye.Tilfeller))
-colnames(cv19)
+# typeof(cv19)
+# cv19ts = as.ts(cv19, deltat=1/365)
+# str(cv19ts)
+# summary(cv19ts)
+# plot(cv19$Dato, log(cv19$Nye.Tilfeller))
+# colnames(cv19)
