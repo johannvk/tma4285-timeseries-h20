193aaf874b22d2c570a444776c127f4e81960e26
ingridsk <47936757+ingridsk@users.noreply.github.com>
2020-11-01 13:02:47 +0100

    Updated finding and testing of GARCH residuals

--- project-2/garch_model.R
+++ project-2/garch_model.R
@@ -11,7 +11,6 @@ library(car)
 library(itsmr)
 
 # Laste data.
-# covid.data = read.csv("C:\\Users\\isskj\\Documents\\NTNU\\Tidsrekker\\Project 2\\covid_ex8.csv", header=TRUE, sep=";")
 covid.data = read.csv("covid_19_new.csv", header=TRUE, sep=";")
 
 
@@ -35,7 +34,6 @@ covid.data$nye.tilf = ts(covid.data$nye.tilf, frequency=7,start = (8+4/7))
 
 
 # Lager en SARIMA-modell ved hjelp av auto.arima. Bruker 7-dagers perioden spesifisert som frekvens i dataene.
-
 sarima_ny = forecast::auto.arima(covid.data$nye.tilf,
                                  lambda="auto",
                                  stepwise=F,
@@ -49,13 +47,11 @@ residuals.new = resid(sarima_ny)
 # Henter ut (p+q) fra sarima-objekt.
 sarima.order = sarima_ny$arma[1]+sarima_ny$arma[2]
 
-
-
 # Ljung-Box test p? residuals fra sarima.
-Box.test(residuals.new, lag=10, type=c("Ljung-Box"), fitdf = sarima.order)
+Box.test(residuals.new, lag=14, type=c("Ljung-Box"), fitdf = sarima.order)
 
 # Ljung-Box p? squared residuals fra sarima. (aka. McLeod-Li)
-Box.test(residuals.new^2, lag=10, type="Ljung-Box", fitdf=sarima.order)
+Box.test(residuals.new^2, lag=14, type="Ljung-Box", fitdf=sarima.order)
 
 
 
@@ -64,25 +60,62 @@ Box.test(residuals.new^2, lag=10, type="Ljung-Box", fitdf=sarima.order)
 # Lager GARCH-modell med normal noise.
 garch.model = ugarchspec(variance.model = list(model="sGARCH", garchOrder = c(0,1)),
                          mean.model = list(armaOrder=c(0,0)),
-                         distribution.model = "norm"
+                         distribution.model = "norm",
+                         fixed.pars=list(mu=0)
                          )
 # Tilpasser garch-modell til residuals fra sarima.
 garch.fit = ugarchfit(garch.model, residuals.new)
-garch.fit
 
+garch_normal_order_sum = 0 + 1
 
 # Lager GARCH-modell med t-noise.
-
 garch.model.t = ugarchspec(variance.model = list(model="sGARCH", garchOrder = c(1,1)),
                            mean.model = list(armaOrder=c(0,0)),
-                           distribution.model = "std"
+                           distribution.model = "std",
+                           fixed.pars=list(mu=0)
                            )
 # Tilpasser garch med t-noise til residuals fra sarima.
 garch.fit.t = ugarchfit(garch.model.t, residuals.new)
 
-# Henter garch residuals.
-residuals_garch_norm = residuals(garch.fit)
-residuals_garch_t = residuals(garch.fit.t)
+garch_t_order_sum = 1 + 1
+
+
+
+# ---------------  Residuals normal-----------------------
+# Henter residuals fra Normal GARCH.
+garch_fitted_norm = sigma(garch.fit)
+residuals_garch_norm = as.vector(residuals.new) - as.vector(garch_fitted_norm)
+
+# Tester
+Box.test(residuals_garch_norm, type="Ljung",lag=14, fitdf = garch_normal_order_sum)
+Box.test(residuals_garch_norm^2, type="Ljung",lag=14, fitdf=garch_normal_order_sum)
+
+# Lage normal qq plot
+qqnorm(residuals_garch_norm, ylab="GARCH residuals", xlab="Theoretical Normal quantiles", main="")
+qqline(residuals_garch_norm)
+
+
+# ------------------ Residuals t -----------------------------
+# Henter residuals fra Student t GARCH
+garch_fitted_t = sigma(garch.fit.t)
+residuals_garch_t = as.vector(residuals.new) - as.vector(garch_fitted_t)
+
+# Test på t residuals
+Box.test(residuals_garch_t, type="Ljung",lag=14, fitdf=garch_t_order_sum)
+Box.test(residuals_garch_t^2, type="Ljung",lag=14, fitdf=garch_t_order_sum)
+
+# Finne frihetsgrader i t-fordeling i garch-modell
+shape_est = garch.fit.t@fit$coef["shape"]
+
+# Lage qq plot vs teoretisk t-fordeling
+qqPlot(residuals_garch_t, distribution="t", df=shape_est, envelope=FALSE, 
+       col.lines="black", lwd=1, ylab="GARCH residuals", xlab="Theoretical t quantiles",
+       grid=FALSE)
+
+
+
+
+
 
 # Tester fra residualene direkte om noen av P-verdiene er mindre
 # enn alpha = 0.05-nivået for Ljung-Box-test ved lags = 1:lag.max.
